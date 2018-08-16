7a3310c2c654c85ea86c0d37e07d6175bc44aa44
zongzi531 <zongzi.xy@gmail.com>
2018-08-16 19:39:40 +0800

    read: src/var/class2type.js

--- src/var/class2type.js
+++ src/var/class2type.js
@@ -3,5 +3,19 @@ define( function() {
 
 	// [[Class]] -> type pairs
   // 返回 空 Object 类型
+  // 用于存放类型，内容如下：
+  // {
+  //   "[object Array]": "array",
+  //   "[object Boolean]": "boolean",
+  //   "[object Date]": "date",
+  //   "[object Error]": "error",
+  //   "[object Function]": "function",
+  //   "[object Number]": "number",
+  //   "[object Object]": "object",
+  //   "[object RegExp]": "regexp",
+  //   "[object String]": "string",
+  //   "[object Symbol]": "symbol"
+  // }
+  // 方法详见 src/core.js "// Populate the class2type map" 注释段
 	return {};
 } );
