dbdf9588e1e7c9aec0b32b7bc43079a1bb7ebaae
zongzi531 <zongzi.xy@gmail.com>
2018-08-16 19:38:59 +0800

    read: src/core DOMEval.js & toType.js

--- src/core/DOMEval.js
+++ src/core/DOMEval.js
@@ -3,6 +3,9 @@ define( [
 ], function( document ) {
 	"use strict";
 
+	// require var/document.js 获得 window.document
+	// 声明 preservedScriptAttributes 对象
+	// 用于遍历已定义的 script 标签属性
 	var preservedScriptAttributes = {
 		type: true,
 		src: true,
@@ -10,12 +13,18 @@ define( [
 	};
 
 	function DOMEval( code, doc, node ) {
+		// 若doc未传入，默认为 document
 		doc = doc || document;
 
+		// 声明遍历属性i
+		// 声明script标签
 		var i,
 			script = doc.createElement( "script" );
 
+		// 将 code 传入 script.text
 		script.text = code;
+		// 若 node 存在，将遍历 preservedScriptAttributes 对象
+		// 并对 node 中存在的 对应属性值 赋值到 新声明的 script 标签上
 		if ( node ) {
 			for ( i in preservedScriptAttributes ) {
 				if ( node[ i ] ) {
@@ -23,8 +32,11 @@ define( [
 				}
 			}
 		}
+		// 在 head 标签内插入 script 标签，然后移除 script 标签
+		// 为什么要这样做呢？
 		doc.head.appendChild( script ).parentNode.removeChild( script );
 	}
 
+	// 返回 DOMEval 方法
 	return DOMEval;
 } );
--- src/core/toType.js
+++ src/core/toType.js
@@ -5,16 +5,48 @@ define( [
 
 "use strict";
 
+  // require var/class2type.js 获得 空 Object 类型
+  // require var/toString.js 获得 Object.prototype.toString 方法
 function toType( obj ) {
+  // 若 obj == null 则将obj转换为 String 类型返回
+  // ==: Abstract Equality 抽象平等
 	if ( obj == null ) {
 		return obj + "";
 	}
 
 	// Support: Android <=2.3 only (functionish RegExp)
+  // 返回 typeof obj === "object" 或
+  //     当 typeof obj === "function" 为 真时，去 class2type 内查找 Object.prototype.toString.call(obj) 为键 是否有值
+  //     若有则返回，若无则返回 "object"
+  //     当 typeof obj === "function" 为 假时，返回 typeof obj
+  // 这个方法在做什么，我也暂时没明白……
+  // 但是能确定的是，不会返回 "function" ，除非 class2type 返回的值是 "function"
+  // 现在看用途大概能知道 class2type 是用来存放一些类型数据的，我相信后面会读到
 	return typeof obj === "object" || typeof obj === "function" ?
 		class2type[ toString.call( obj ) ] || "object" :
 		typeof obj;
 }
 
+  // 返回 toType 方法
 return toType;
 } );
+
+// define( [
+//   "../var/class2type",
+//   "../var/toString"
+// ], function( class2type, toString ) {
+//   "use strict";
+
+//   function toType( obj ) {
+//     if ( obj == null ) {
+//       return obj + "";
+//     }
+
+//     // Support: Android <=2.3 only (functionish RegExp)
+//     return typeof obj === "object" || typeof obj === "function" ?
+//       class2type[ toString.call( obj ) ] || "object" :
+//       typeof obj;
+//   }
+
+//   return toType;
+// } );
