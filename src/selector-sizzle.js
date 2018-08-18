8ea0ff0f6fc091e613afe1c6d9e54c63c002c606
zongzi531 <zongzi.xy@gmail.com>
2018-08-18 10:03:23 +0800

    read: src/selector.js & src/selector-sizzle.js

--- src/selector-sizzle.js
+++ src/selector-sizzle.js
@@ -4,7 +4,15 @@ define( [
 ], function( jQuery, Sizzle ) {
 
 "use strict";
+  // require core.js 获得 jQuery
+  // require external/sizzle/dist/sizzle.js 获得 Sizzle
 
+  // Sizzle
+  // A pure-JavaScript CSS selector engine designed to be easily dropped in to a host library.
+  // 一个纯粹的JavaScript CSS选择器引擎，它被设计成可以轻松地进入主机库。
+  // GitHub: https://github.com/jquery/sizzle
+
+  // 为 jQuery 添加属性
 jQuery.find = Sizzle;
 jQuery.expr = Sizzle.selectors;
 
--- src/selector.js
+++ src/selector.js
@@ -1,3 +1,5 @@
 define( [ "./selector-sizzle" ], function() {
 	"use strict";
+
+  // require selector-sizzle.js
 } );
