01989cb4423aaa64498a7148f87b0d567b8a363f
zongzi531 <zongzi.xy@gmail.com>
2018-08-18 10:04:23 +0800

    read: src/traversing/var/rneedsContext.js

--- src/traversing/var/rneedsContext.js
+++ src/traversing/var/rneedsContext.js
@@ -4,5 +4,10 @@ define( [
 ], function( jQuery ) {
 	"use strict";
 
+  // require core.js 获得 jQuery
+  // require selector.js
+
+  // 返回 jQuery.expr.match.needsContext
+  // jQuery.expr = Sizzle.selectors
 	return jQuery.expr.match.needsContext;
 } );
