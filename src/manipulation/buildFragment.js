31ddb56ed7e8bca50fcf8fdd0d5f6cd27b0aacc6
zongzi531 <zongzi.xy@gmail.com>
2018-08-20 20:30:30 +0800

    read: src/manipulation/buildFragment.js

--- src/manipulation/buildFragment.js
+++ src/manipulation/buildFragment.js
@@ -9,10 +9,25 @@ define( [
 ], function( jQuery, toType, rtagName, rscriptType, wrapMap, getAll, setGlobalEval ) {
 
 "use strict";
+	// require core.js 获得 jQuery
+	// require core/toType.js 获得 toType 方法
+	// require ./var/rtagName.js 获得 正则表达式
+	// require ./var/rscriptType.js 获得 检查模块化 script 标签的正则表达式
+	// require ./wrapMap.js 获得 wrapMap 对象
+	// require ./getAll.js 获得 getAll 方法
+	// require ./setGlobalEval.js 获得 setGlobalEval 方法
 
+// 声明 rhtml 正则表达式
 var rhtml = /<|&#?\w+;/;
 
+// 声明 buildFragment 方法
 function buildFragment( elems, context, scripts, selection, ignored ) {
+	// 初始化 elem, tmp, tag, wrap, contains, j
+	// 声明 fragment 创建一个新的空白的文档片段( DocumentFragment)
+	// 声明 nodes = 空数组
+	// 声明 i = 0
+	// 声明 l = 元素集长度
+	// 先读到这里把
 	var elem, tmp, tag, wrap, contains, j,
 		fragment = context.createDocumentFragment(),
 		nodes = [],
