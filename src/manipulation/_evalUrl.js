0a10f76dd79767b38caebd20c6c178f1b112d202
zongzi531 <zongzi.xy@gmail.com>
2018-09-17 20:05:00 +0800

    read: src/manipulation/_evalUrl.js

--- src/manipulation/_evalUrl.js
+++ src/manipulation/_evalUrl.js
@@ -4,11 +4,17 @@ define( [
 
 "use strict";
 
+	// require ajax.js 获得 jQuery
+
+// jQuery._evalUrl 赋值 匿名函数
 jQuery._evalUrl = function( url ) {
+
+	// 返回 jQuery.ajax 执行 结果
 	return jQuery.ajax( {
 		url: url,
 
 		// Make this explicit, since user can override this through ajaxSetup (#11264)
+		// 使这一点明确，因为用户可以通过 ajaxSetup 来覆盖这个
 		type: "GET",
 		dataType: "script",
 		cache: true,
@@ -18,6 +24,7 @@ jQuery._evalUrl = function( url ) {
 	} );
 };
 
+// 返回 jQuery._evalUrl
 return jQuery._evalUrl;
 
 } );
