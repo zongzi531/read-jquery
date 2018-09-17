0b7a19f791404b8ef42fb5b34603cc90969da749
zongzi531 <zongzi.xy@gmail.com>
2018-09-17 20:04:33 +0800

    read: src/exports folder

--- src/exports/amd.js
+++ src/exports/amd.js
@@ -4,6 +4,8 @@ define( [
 
 "use strict";
 
+	// require core.js 获得 jQuery
+
 // Register as a named AMD module, since jQuery can be concatenated with other
 // files that may use define, but not via a proper concatenation script that
 // understands anonymous AMD modules. A named AMD is safest and most robust
@@ -11,14 +13,21 @@ define( [
 // derived from file names, and jQuery is normally delivered in a lowercase
 // file name. Do this after creating the global so that if an AMD module wants
 // to call noConflict to hide this version of jQuery, it will work.
+// 注册为命名的AMD模块，因为jQuery可以与可以使用define的其他文件连接，但不能通过理解匿名AMD模块的正确连接脚本连接。命名AMD是最安全和最稳健的注册方式。之所以使用小写jquery，是因为AMD模块名称来源于文件名，而jQuery通常以小写文件名传递。在创建全局后这样做，这样如果AMD模块想调用NoRebug来隐藏这个jQuery版本，它会起作用。
 
 // Note that for maximum portability, libraries that are not jQuery should
 // declare themselves as anonymous modules, and avoid setting a global if an
 // AMD loader is present. jQuery is a special case. For more information, see
 // https://github.com/jrburke/requirejs/wiki/Updating-existing-libraries#wiki-anon
+// 注意，为了实现最大的可移植性，不是jQuery的库应该声明自己为匿名模块，并且如果存在AMD加载器，则避免设置全局模块。jQuery是一个特例。
 
+// 判断 typeof define === "function" && define.amd
 if ( typeof define === "function" && define.amd ) {
+
+	// 执行 define 方法
 	define( "jquery", [], function() {
+
+		// 返回 jQuery
 		return jQuery;
 	} );
 }
--- src/exports/global.js
+++ src/exports/global.js
@@ -4,30 +4,50 @@ define( [
 
 "use strict";
 
+	// require core.js 获得 jQuery
+	// noGlobal 赋值 undefined
+
 var
 
 	// Map over jQuery in case of overwrite
+	// 在重写的情况下映射 jQuery
+	// 声明 _jQuery 赋值 window.jQuery
 	_jQuery = window.jQuery,
 
 	// Map over the $ in case of overwrite
+	// 在重写的情况下映射 $
+	// 声明 _$ 赋值 window.$
 	_$ = window.$;
 
+// jQuery.noConflict 赋值 匿名函数
 jQuery.noConflict = function( deep ) {
+
+	// 判断 window.$ === jQuery
 	if ( window.$ === jQuery ) {
+
+		// window.$ 赋值 _$
 		window.$ = _$;
 	}
 
+	// 判断 deep && window.jQuery === jQuery
 	if ( deep && window.jQuery === jQuery ) {
+
+		// window.jQuery 赋值 _jQuery
 		window.jQuery = _jQuery;
 	}
 
+	// 返回 jQuery
 	return jQuery;
 };
 
 // Expose jQuery and $ identifiers, even in AMD
 // (#7102#comment:10, https://github.com/jquery/jquery/pull/557)
 // and CommonJS for browser emulators (#13566)
+// 公开jQuery和$标识符，即使在AMD和公共浏览器中也适用于浏览器仿真器
+// 判断 !noGlobal
 if ( !noGlobal ) {
+
+	// window.jQuery 和 window.$ 赋值 jQuery
 	window.jQuery = window.$ = jQuery;
 }
 
