74941fd1f0dc8c3a98632f57246ec0bb855c7a30
zongzi531 <zongzi.xy@gmail.com>
2018-09-17 20:03:39 +0800

    read: src/effects/animatedSelector.js

--- src/effects/animatedSelector.js
+++ src/effects/animatedSelector.js
@@ -6,8 +6,17 @@ define( [
 
 "use strict";
 
+	// require core.js 获得 jQuery
+	// require selector.js
+	// require effects.js
+
+// jQuery.expr.pseudos.animated 赋值 匿名函数
 jQuery.expr.pseudos.animated = function( elem ) {
+
+	// 返回 jQuery.grep 方法 返回的 长度
 	return jQuery.grep( jQuery.timers, function( fn ) {
+
+		// 返回 elem === fn.elem
 		return elem === fn.elem;
 	} ).length;
 };
